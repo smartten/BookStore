566b5ca96b998649fc6feaf352b14fa1a3c6cf6f
botapdem <duyanhkakashi@gmail.com>
2024-08-26 10:13:12 +0700

    Thêm file mới và cập nhật nội dung

---
+++ my-app/src/pages/users/homepage/index.js
@@ -0,0 +1,25 @@
+import { memo } from "react";
+
+const Homepage = () => {
+    return (
+      <>
+      
+      <div class="container">
+  <div class="row">
+    <div class="col-sm">
+     
+    </div>
+    <div class="col-sm">
+    
+    </div>
+    <div class="col-sm">
+   
+    </div>
+  </div>
+</div>
+
+      </>
+    );
+}; 
+
+export default memo(Homepage);
